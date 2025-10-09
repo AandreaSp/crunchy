2ef82a96e13cb023185582ce1dbd173eb152e3cf
AandreaSp <a.santoplo1@studenti.unimol.it>
2025-10-09 15:38:39 +0200

    creazione home

---
+++ lib/bottom_bar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+//import 'menu.dart';
+//import 'location.dart';
+//import 'info.dart';
+
+class CrunchyBottomBar extends StatefulWidget {
+  const CrunchyBottomBar({super.key});
+
+  @override
+  State<CrunchyBottomBar> createState() => _CrunchyShellState();
+}
+
+class _CrunchyShellState extends State<CrunchyBottomBar> {
+  int _currentIndex = 0;
+
+  final _pages = const [
+    HomePage(),
+    // MenuPage(),
+    // LocationPage(),
+    // InfoPage(),
+  ];
+
+  final _titles = const ['Home', 'Menu', 'Location', 'Info'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(_titles[_currentIndex])),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (i) => setState(() => _currentIndex = i),
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            tooltip: 'Vai alla Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book),
+            label: 'Menu',
+            tooltip: 'Guarda il menù',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.place),
+            label: 'Ristorante',
+            tooltip: 'Trova ristorante',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'Info',
+            tooltip: 'Informazioni',
+          ),
+        ],
+      ),
+    );
+  }
+}

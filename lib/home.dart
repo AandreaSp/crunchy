e4488e14bba7d0201ed2da05117d326b497f5c5c
AandreaSp <a.santoplo1@studenti.unimol.it>
2025-10-09 22:13:36 +0200

    aggiunta immagine della home

--- lib/home.dart
+++ lib/home.dart
@@ -5,7 +5,19 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+  padding: const EdgeInsets.all(30),
+  child: ClipRRect(
+    borderRadius: BorderRadius.circular(16),
+    child: Image.asset('asset/home_image.png', height: 200, fit: BoxFit.cover),
+  ),
+)
+          ],
+        ),
+      ),
     );
   }
 }

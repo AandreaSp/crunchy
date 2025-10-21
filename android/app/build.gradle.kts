92ded501a057104c0d0486fb2bd748378ecd0c3a
Kevin Cerio <k.cerio@studenti.unimol.it>
2025-10-21 17:25:26 +0200

    cambiata la configurazione dell'utilizzo delle chiavi

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,15 +1,25 @@
 import java.util.Properties
-import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
-apply(from = "generate_secrets.gradle.kts")
+val secretsProps = Properties().apply {
+    val f = rootProject.file("android/secrets.properties")
+    if (f.exists()) {
+        f.inputStream().use { load(it) }
+    }
+}
+
+val MAPS_API_KEY: String = secretsProps.getProperty("MAPS_API_KEY")
+    ?: System.getenv("MAPS_API_KEY")
+    ?: (project.findProperty("MAPS_API_KEY") as String?) ?: ""
+
+val NEWS_API_KEY: String = secretsProps.getProperty("NEWS_API_KEY")
+    ?: System.getenv("NEWS_API_KEY")
+    ?: (project.findProperty("NEWS_API_KEY") as String?) ?: ""
 
 android {
     namespace = "com.example.crunchy"
@@ -26,20 +36,19 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.crunchy"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        manifestPlaceholders += mapOf(
+            "MAPS_API_KEY" to MAPS_API_KEY
+        )
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -47,12 +56,4 @@ android {
 
 flutter {
     source = "../.."
-}
-
-secrets {
-    propertiesFileName = "secrets.properties"
-}
-
-tasks.named("preBuild") {
-    dependsOn("generateDartSecrets")
 }
